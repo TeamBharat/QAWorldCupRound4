bbe03b871825cee034fbf125d9519845b8a78b7b
tarunnamdev <tarun.namdev@nagarro.com>
2022-05-25 18:55:06 +0530

    added NQLB scenario

--- SpecFlowProjectTest/PageObjects/NQLBCommunityPage.cs
+++ SpecFlowProjectTest/PageObjects/NQLBCommunityPage.cs
@@ -48,8 +48,27 @@ namespace SpecFlowProjectTest.PageObjects
         }
         public void MemberNameList()
         {
-            
-        }
 
+            Dictionary<string, string> map = new Dictionary<string, string>();
+            int j = 1;
+
+            for (int i = 0; i <= MemberName.Count(); i++)
+            {
+                map.Add(MemberName[i].Text, MemberEmailId[i].Text);
+            }
+            utilities.SetCellData("Sheetname", 0, 0, "MemberName");
+            utilities.SetCellData("Sheetname", 0, 1, "Member Email");
+
+            foreach (KeyValuePair<string, string> community in map)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}",
+                community.Key, community.Value);
+                utilities.SetCellData("Sheetname", j, 1, community.Key);
+                utilities.SetCellData("Sheetname", j, 2, community.Value.ToString());
+                j = j + 1;
+            }
+            utilities.CloseExcel();
+        }
+      
     }
 }
